5e4b0b99ecb52026ea517337048f1bb4e95cd21a
john-papani <johnpapani1@gmail.com>
2025-05-22 20:16:59 +0300

    Add vouli photo bg in main page

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,19 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-
+import vouli from "../../public/vouli.jpg"; // Adjust the path as necessary
 export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div className="min-h-[85vh] bg-gradient-to-br from-white via-slate-100 to-slate-200 flex items-center justify-center px-4">
+    <div className="min-h-[85vh] flex items-center justify-center px-4">
+      <Image
+        width={1000}
+        height={1000}
+        src={vouli}
+        alt="Greek Flag"
+        className="absolute top-0 left-0 w-full h-full z-[-1] object-cover pt-16 opacity-15"
+      />
       <div className="max-w-2xl w-full text-center py-16">
         <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 mb-6">
           Το Σύνταγμα της Ελλάδας
@@ -17,12 +24,13 @@ export default function HomePage() {
           Ανακάλυψε το θεμέλιο του πολιτεύματος της χώρας μας.
         </p>
         <p className="text-base text-gray-500 mb-8">
-          Περιηγήσου στα άρθρα του Συντάγματος, κατανόησε τα δικαιώματα και τις υποχρεώσεις που καθορίζουν τη λειτουργία της Ελληνικής Δημοκρατίας.
+          Περιηγήσου στα άρθρα του Συντάγματος, κατανόησε τα δικαιώματα και τις
+          υποχρεώσεις που καθορίζουν τη λειτουργία της Ελληνικής Δημοκρατίας.
         </p>
 
         <button
           onClick={() => router.push("/syntagma")}
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-8 rounded-xl shadow-md transition duration-300"
+          className="bg-blue-600 hover:bg-blue-800 text-white font-semibold py-3 px-8 rounded-xl shadow-md transition duration-300 hover:cursor-pointer"
         >
           Δες τα Άρθρα
         </button>
